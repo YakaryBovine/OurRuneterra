a2e08bc823cc7255cf810a2721038803ed9da303
Zakary Boven <zakaryboven@gmail.com>
2024-01-20 12:45:13 +1300

    Added test to ensure Regeneration doesn't apply while in hand

---
+++ OurRuneterra.Core.UnitTests/KeywordTests/RegenerationTests.cs
@@ -0,0 +1,48 @@
+﻿using OurRuneterra.Core.Behaviours;
+using OurRuneterra.Core.Cards;
+using OurRuneterra.Core.Keywords;
+using OurRuneterra.Core.Tests.TestHelpers;
+
+namespace OurRuneterra.Core.Tests.KeywordTests;
+
+public sealed class RegenerationTests
+{
+  [Fact]
+  public void Regeneration_Unit_On_Board_Heals_At_End_Of_Round()
+  {
+    var game = Utils.StartSimpleGame();
+    var testPlayer = game.Players.First();
+    var regenerationUnit = game.Summon(testPlayer, new Unit("Cithria of Cloudfield", 2, 2, 0, Region.Demacia)
+    {
+      CurrentHealth = 1,
+      PassiveEffects = new List<PassiveEffect>
+      {
+        new Regeneration()
+      }
+    }) as Unit;
+
+    game.EndRound(testPlayer);
+
+    regenerationUnit!.CurrentHealth.Should().Be(2);
+  }
+
+  [Fact]
+  public void Regeneration_Unit_In_Hand_Doesnt_Heal_At_End_Of_Round()
+  {
+    var regenerationUnit = new Unit("Cithria of Cloudfield", 2, 2, 0, Region.Demacia)
+    {
+      CurrentHealth = 1,
+      PassiveEffects = new List<PassiveEffect>
+      {
+        new Regeneration()
+      }
+    };
+    var game = Utils.StartSimpleGame();
+    var testPlayer = game.Players.First();
+    game.CreateInHand(testPlayer, regenerationUnit);
+
+    game.EndRound(testPlayer);
+
+    regenerationUnit.CurrentHealth.Should().Be(1);
+  }
+}
--- OurRuneterra.Core.UnitTests/KeywordTests/ToughTests.cs
+++ OurRuneterra.Core.UnitTests/KeywordTests/ToughTests.cs
@@ -3,9 +3,9 @@ using OurRuneterra.Core.Cards;
 using OurRuneterra.Core.Keywords;
 using OurRuneterra.Core.Tests.TestHelpers;
 
-namespace OurRuneterra.Core.Tests;
+namespace OurRuneterra.Core.Tests.KeywordTests;
 
-public sealed class KeywordTests
+public sealed class ToughTests
 {
   [Fact]
   public void Tough_Unit_Takes_One_Less_Damage()
@@ -24,23 +24,5 @@ public sealed class KeywordTests
     game.Damage(damager!, victim!, 2);
 
     victim!.CurrentHealth.Should().Be(1);
-  }
-
-  [Fact]
-  public void Regeneration_Unit_Heals_At_End_Of_Round()
-  {
-    var game = Utils.StartSimpleGame();
-    var regenerationUnit = new Unit("Cithria of Cloudfield", 2, 2, 0, Region.Demacia)
-    {
-      CurrentHealth = 1
-    };
-    regenerationUnit.PassiveEffects.Add(new Regeneration());
-    var testPlayer = game.Players.First();
-    testPlayer.Hand.Add(regenerationUnit);
-    game.PlaceCard(testPlayer, regenerationUnit);
-    
-    game.EndRound(testPlayer);
-
-    regenerationUnit.CurrentHealth.Should().Be(2);
   }
 }
